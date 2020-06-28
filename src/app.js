7fb6e0b058273ac8f40c981907a1e2eca7564128
jemsantos <eustaquio.cadastros@gmail.com>
2020-06-28 17:02:04 -0300

    Código final após aula 04

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const http = require('http')
 
 const app = express()
- 
+
 const hostname = '127.0.0.1';
 const port = 3000;
 
@@ -12,9 +12,9 @@ app.use((request, response, next) => {
   //response.status(404).send('Página não encontrada!')
   response.status(404).send()
 });
- 
+
 const server = http.createServer(app);
- 
+
 server.listen(port, hostname, () => {
   console.log(`Servidor em execução em http://${hostname}:${port}/`);
 });
